70ef4cdd75cfbc64c0ef359c24142d0b00a40b1f
Grzegorz Gruza <ggruza@nvidia.com>
2024-04-25 06:55:52 -0700

    verify_mounts.sh

---
+++ client/get-mount-info.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if ! [ "$#" -eq 2 ]; then
+    echo "Error: get-mount-info.sh takes 2 arguments: 1) paths to verify 2) output path"
+    exit 1
+fi
+
+OLD_IFS=$IFS
+IFS=","
+for dir_path in $1; do
+    if ! [ -d "$dir_path" ]; then
+        echo "Error: path $dir_path is incorrect"
+        exit 1
+    fi
+done
+
+exec > "$2/cont-mount-info.sh"
+echo "declare -a directory_sizes"
+echo "declare -a number_of_paths_in_dir"
+
+for dir_path in $1; do
+    echo "# ----------"
+    echo "directory_sizes+=("
+    dir_size=$(du -s "$dir_path" | cut -f1)
+    echo "\",$dir_size\""
+    dir_counter=1
+    while IFS= read -r subdir; do
+        relative_path="${subdir#$dir_path/}"
+        subdir_size=$(du -s "$subdir" | cut -f1)
+        echo "\"$relative_path,$subdir_size\""
+        ((dir_counter++))
+    done < <(find "$dir_path" -mindepth 1 -type d | awk -v dir_path="$dir_path" -F'/' '{print NF-1, $0}' | sort -n | cut -d' ' -f2-)
+    echo ")"
+    echo "number_of_paths_in_dir+=($dir_counter)"
+done
+IFS=$OLD_IFS
---
+++ client/verify-mounts.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Error: init_config.sh takes 1 or 2 arguments: 1) paths to verify 2) path to file with mount check info"
+    exit 1
+fi
+
+if [ -n "$2" ]; then
+    config_file="$2"
+else
+    config_file="cont-mount-info.sh"
+fi
+
+if ! [ -f "$config_file" ]; then
+    echo "Error: config file path $config_file is incorrect"
+    exit 1
+fi
+
+source $config_file
+
+OLD_IFS=$IFS
+IFS=","
+for dir_path in $1; do
+    if ! [ -d "$dir_path" ]; then
+        echo "Error: path $dir_path is incorrect"
+        exit 1
+    fi
+done
+
+index=0
+directory_sizes_counter=0
+directory_sizes_end=0
+for dir_path in $1; do
+
+    ((directory_sizes_end += ${number_of_paths_in_dir[$index]}))
+
+
+    for ((; directory_sizes_counter < directory_sizes_end; directory_sizes_counter++)); do
+        subdir_and_size=${directory_sizes[$directory_sizes_counter]}
+        read -r subdir_to_check subdir_size_gt <<< "$subdir_and_size"
+        subdir_to_check="$dir_path/$subdir_to_check"
+        subdir_to_check=${subdir_to_check//\/\//\/} # replace doubled slashes with single slash
+
+        if ! [ -d "$subdir_to_check" ]; then
+            echo "Error: $dir_path is incorrectly initialized. Path $subdir_to_check is missing"
+            directory_sizes_counter=$directory_sizes_end
+            break
+        fi
+
+        subdir_size=$(du -s "$subdir_to_check" | cut -f1)
+
+        if [ "$subdir_size_gt" != "$subdir_size" ]; then
+            echo "Error: $dir_path is incorrectly initialized. Bad size of $subdir_to_check"
+            directory_sizes_counter=$directory_sizes_end
+            break
+        fi
+    done
+    ((index++))
+done
+echo "DIR CHECK OK" # TODO: REMOVE AFTER TESTS 
+IFS=$OLD_IFS
