21e892b8c50499d665dd57630b7d503fa66f30bd
Grzegorz Gruza <ggruza@nvidia.com>
2024-05-07 07:30:21 -0700

    mount info

--- client/get-mount-info.sh
+++ client/get-mount-info.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 if ! [ "$#" -eq 2 ]; then
     echo "Error: get-mount-info.sh takes 2 arguments: 1) paths to verify 2) output path"
     exit 1
@@ -14,6 +13,8 @@ for dir_path in $1; do
     fi
 done
 
+mkdir -p "$2"
+touch "$2/cont-mount-info.sh"
 exec > "$2/cont-mount-info.sh"
 echo "declare -a directory_sizes"
 echo "declare -a number_of_paths_in_dir"
@@ -21,15 +22,17 @@ echo "declare -a number_of_paths_in_dir"
 for dir_path in $1; do
     echo "# ----------"
     echo "directory_sizes+=("
-    dir_size=$(du -s "$dir_path" | cut -f1)
-    echo "\",$dir_size\""
+    dir_size=$(du -sk "$dir_path" | cut -f1)
+    num_files=$(ls -1 "$dir_path" | wc -l)
+    echo "\",$dir_size,$num_files\""
     dir_counter=1
     while IFS= read -r subdir; do
         relative_path="${subdir#$dir_path/}"
-        subdir_size=$(du -s "$subdir" | cut -f1)
-        echo "\"$relative_path,$subdir_size\""
+        subdir_size=$(du -sk "$subdir" | cut -f1)
+        num_files=$(ls -1 "$subdir" | wc -l)
+        echo "\"$relative_path,$subdir_size,$num_files\""
         ((dir_counter++))
-    done < <(find "$dir_path" -mindepth 1 -type d | awk -v dir_path="$dir_path" -F'/' '{print NF-1, $0}' | sort -n | cut -d' ' -f2-)
+    done < <(find "$dir_path" -mindepth 1 -type d ! -path '*/.*' | awk -v dir_path="$dir_path" -F'/' '{print NF-1, $0}' | sort -n | cut -d' ' -f2-)
     echo ")"
     echo "number_of_paths_in_dir+=($dir_counter)"
 done
