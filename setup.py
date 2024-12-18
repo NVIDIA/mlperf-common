c6f3aa5491fa451b8d0e321500e07e4143e44747
Matt Frank <mfrank@nvidia.com>
2024-12-18 10:43:13 -0800

    compile fastcp

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
+# Copyright (c) 2022-2024, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,9 +15,30 @@
 import sys
 
 from setuptools import find_packages, setup
+from setuptools.command.build import build as _build
+import subprocess
+import os
 
 with open("README.md") as readme_file:
     readme = readme_file.read()
+
+class BuildWithCppCommand(_build):
+    """Custom build command to compile C++ code before the standard build."""
+
+    def run(self):
+        # Compile the C++ source code
+        sources = ["src/fastcp.cpp", "src/cmdline.cpp"]
+        output = "bin/fastcp"
+
+        # Ensure the 'bin' directory exists
+        os.makedirs("bin", exist_ok=True)
+
+        # Compile the C++ code
+        print(f"Compiling {sources} to {output}")
+        subprocess.check_call(["g++", "-O3", "-Wall", "-lpthread", *sources, "-o", output])
+
+        # Run the standard build process
+        _build.run(self)
 
 
 setup(
@@ -31,6 +52,12 @@ setup(
     name="mlperf-common",
     packages=find_packages(where="."),
     scripts=['client/bindpcie','client/slurm2pytorch','client/mgpurun','client/mlperf-sysjson.sh'],
+    data_files=[
+        ("bin", ["bin/fastcp"]),
+    ],
+    cmdclass={
+        "build": BuildWithCppCommand,
+    },
     url="https://github.com/NVIDIA/mlperf-common",
     version="0.3",
 )
