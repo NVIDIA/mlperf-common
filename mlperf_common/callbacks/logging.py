7ca911617c94ccfb713b48c56416f613d1eecf33
Jan Baczek <jbaczek@nvidia.com>
2025-07-01 13:17:18 +0200

    Typo

--- mlperf_common/callbacks/logging.py
+++ mlperf_common/callbacks/logging.py
@@ -144,7 +144,7 @@ class LoggingCallback(pl.Callback):
         pl_module: pl.LightningModule,
     ) -> None:
         if not trainer.warmup:
-            log_custom_timedelta("validation_time")
+            self.log_custom_timedelta("validation_time")
             train_batch_size = self.get_train_step_samples_count(trainer, pl_module)
             validation_samples = self.get_validation_samples_count(trainer, pl_module)
 
